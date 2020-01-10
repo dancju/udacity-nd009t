e2bbdf50bdc8168e906d98bd3d5dfcca458a7259
daniel <danichau93@gmail.com>
2020-01-11 00:57:38 +0800

    change learning target to "log diff"

--- capstone/src/evaluator.py
+++ capstone/src/evaluator.py
@@ -14,6 +14,152 @@ def _read_csv(input_dir: str, filename: str) -> pd.DataFrame:
     )
 
 
+# def evaluate(input_dir: str, output_dir: str) -> None:
+#     pd.plotting.register_matplotlib_converters()
+#     datasets = ["trai", "vali", "test"]
+#     models = ["linear", "lstm"]
+#     y = {}
+#     pred = {}
+#     loss = {}
+#     for d in datasets:
+#         y[d] = _read_csv(input_dir, f"{d}_y.csv")
+#     for m in models:
+#         pred[m] = pd.concat(
+#             [_read_csv(output_dir, f"pred_{d}_{m}.csv") for d in datasets]
+#         )
+#         loss[m] = pd.concat(
+#             [_read_csv(output_dir, f"loss_{d}_{m}.csv") for d in datasets]
+#         )
+#     fig, axs = plt.subplots(nrows=2, ncols=2, figsize=(16, 9))
+#     axs[0, 0].plot(pd.concat([y[d] for d in datasets]))
+#     axs[0, 1].plot(pd.concat([y[d] for d in datasets]))
+#     axs[0, 0].plot(
+#         pred["linear"], linestyle="", marker=".", markersize=0.1,
+#     )
+#     axs[0, 1].plot(
+#         pred["lstm"], linestyle="", marker=".", markersize=0.1,
+#     )
+#     axs[1, 0].fill_between(loss["linear"].index, loss["linear"])
+#     axs[1, 1].fill_between(loss["lstm"].index, loss["lstm"])
+#     axs[0, 0].axvline(y["vali"].index[0], linestyle=":", c="black")
+#     axs[0, 0].axvline(y["test"].index[0], linestyle=":", c="black")
+#     axs[1, 0].axvline(y["vali"].index[0], linestyle=":", c="black")
+#     axs[1, 0].axvline(y["test"].index[0], linestyle=":", c="black")
+#     axs[0, 1].axvline(y["vali"].index[0], linestyle=":", c="black")
+#     axs[0, 1].axvline(y["test"].index[0], linestyle=":", c="black")
+#     axs[1, 1].axvline(y["vali"].index[0], linestyle=":", c="black")
+#     axs[1, 1].axvline(y["test"].index[0], linestyle=":", c="black")
+#     # axs[1, 0].set_ylim(0, 0.35)
+#     axs[1, 1].set_ylim(axs[1, 0].get_ylim())
+#     axs[0, 1].legend(["truth", "prediction"])
+#     axs[1, 0].text(
+#         date2num(y["trai"].index[y["trai"].shape[0] // 2]),
+#         0.9 * axs[1, 0].get_ylim()[1],
+#         "training",
+#         horizontalalignment="center",
+#     )
+#     axs[1, 0].text(
+#         date2num(y["vali"].index[y["vali"].shape[0] // 2]),
+#         0.9 * axs[1, 0].get_ylim()[1],
+#         "validation",
+#         horizontalalignment="center",
+#     )
+#     axs[1, 0].text(
+#         date2num(y["test"].index[y["test"].shape[0] // 2]),
+#         0.9 * axs[1, 0].get_ylim()[1],
+#         "testing",
+#         horizontalalignment="center",
+#     )
+#     axs[1, 1].text(
+#         date2num(y["trai"].index[y["trai"].shape[0] // 2]),
+#         0.9 * axs[1, 0].get_ylim()[1],
+#         "training",
+#         horizontalalignment="center",
+#     )
+#     axs[1, 1].text(
+#         date2num(y["vali"].index[y["vali"].shape[0] // 2]),
+#         0.9 * axs[1, 0].get_ylim()[1],
+#         "validation",
+#         horizontalalignment="center",
+#     )
+#     axs[1, 1].text(
+#         date2num(y["test"].index[y["test"].shape[0] // 2]),
+#         0.9 * axs[1, 0].get_ylim()[1],
+#         "testing",
+#         horizontalalignment="center",
+#     )
+#     axs[0, 0].set_xticks([])
+#     axs[0, 1].set_xticks([])
+#     axs[1, 0].set_xlabel("time")
+#     axs[1, 1].set_xlabel("time")
+#     axs[0, 0].set_ylabel("normalised price")
+#     axs[1, 0].set_ylabel("loss")
+#     axs[0, 0].set_title("Linear Model")
+#     axs[0, 1].set_title("LSTM Model")
+#     fig.subplots_adjust(hspace=0)
+
+
+def _plot_scatters(
+    output_dir: str, datasets: list, models: list, y: dict, pred: dict
+) -> None:
+    fig, axs = plt.subplots(nrows=2, ncols=3, figsize=(16, 9))
+    for di, d in zip(range(3), datasets):
+        for mi, m in zip(range(2), models):
+            axs[mi, di].scatter(y[d], pred[d, m])
+    for ax in fig.get_axes():
+        ax.label_outer()
+    axs[1, 0].set_xlabel("train")
+    axs[1, 1].set_xlabel("train")
+    axs[1, 2].set_xlabel("train")
+    axs[0, 0].set_ylabel("linear")
+    axs[1, 0].set_ylabel("LSTM")
+    plt.savefig(os.path.join(output_dir, "evaluate_scatters.png"))
+
+
+def _plot_scatter(
+    output_dir: str, datasets: list, models: list, y: dict, pred: dict
+) -> None:
+    fig, ax = plt.subplots(figsize=(16, 9))
+    for m in models:
+        ax.scatter(y["test"], pred["test", m])
+    ax.legend(models)
+    ax.set_xlabel("truth")
+    ax.set_ylabel("prediction")
+    plt.savefig(os.path.join(output_dir, "evaluate_scatter.png"))
+
+
+def _plot_loss(
+    output_dir: str, datasets: list, models: list, y: dict, loss: dict
+) -> None:
+    fig, ax = plt.subplots(figsize=(16, 9))
+    ax.plot(pd.concat([loss[d, "linear"] for d in datasets]))
+    ax.plot(pd.concat([loss[d, "lstm"] for d in datasets]))
+    ax.axvline(y["vali"].index[0], linestyle=":", c="black")
+    ax.axvline(y["test"].index[0], linestyle=":", c="black")
+    ax.text(
+        date2num(y["trai"].index[y["trai"].shape[0] // 2]),
+        0.9 * ax.get_ylim()[1],
+        "training",
+        horizontalalignment="center",
+    )
+    ax.text(
+        date2num(y["vali"].index[y["vali"].shape[0] // 2]),
+        0.9 * ax.get_ylim()[1],
+        "validation",
+        horizontalalignment="center",
+    )
+    ax.text(
+        date2num(y["test"].index[y["test"].shape[0] // 2]),
+        0.9 * ax.get_ylim()[1],
+        "testing",
+        horizontalalignment="center",
+    )
+    ax.legend(models)
+    ax.set_xlabel("time")
+    ax.set_ylabel("MSE")
+    fig.savefig(os.path.join(output_dir, "evaluate_loss.png"))
+
+
 def evaluate(input_dir: str, output_dir: str) -> None:
     pd.plotting.register_matplotlib_converters()
     datasets = ["trai", "vali", "test"]
@@ -24,76 +170,8 @@ def evaluate(input_dir: str, output_dir: str) -> None:
     for d in datasets:
         y[d] = _read_csv(input_dir, f"{d}_y.csv")
     for m in models:
-        pred[m] = pd.concat(
-            [_read_csv(output_dir, f"pred_{d}_{m}.csv") for d in datasets]
-        )
-        loss[m] = pd.concat(
-            [_read_csv(output_dir, f"loss_{d}_{m}.csv") for d in datasets]
-        )
-    fig, axs = plt.subplots(nrows=2, ncols=2, figsize=(16, 9))
-    axs[0, 0].plot(pd.concat([y[d] for d in datasets]))
-    axs[0, 1].plot(pd.concat([y[d] for d in datasets]))
-    axs[0, 0].plot(
-        pred["linear"], linestyle="", marker=".", markersize=0.1,
-    )
-    axs[0, 1].plot(
-        pred["lstm"], linestyle="", marker=".", markersize=0.1,
-    )
-    axs[1, 0].fill_between(loss["linear"].index, loss["linear"])
-    axs[1, 1].fill_between(loss["lstm"].index, loss["lstm"])
-    axs[0, 0].axvline(y["vali"].index[0], linestyle=":", c="black")
-    axs[0, 0].axvline(y["test"].index[0], linestyle=":", c="black")
-    axs[1, 0].axvline(y["vali"].index[0], linestyle=":", c="black")
-    axs[1, 0].axvline(y["test"].index[0], linestyle=":", c="black")
-    axs[0, 1].axvline(y["vali"].index[0], linestyle=":", c="black")
-    axs[0, 1].axvline(y["test"].index[0], linestyle=":", c="black")
-    axs[1, 1].axvline(y["vali"].index[0], linestyle=":", c="black")
-    axs[1, 1].axvline(y["test"].index[0], linestyle=":", c="black")
-    # axs[1, 0].set_ylim(0, 0.35)
-    axs[1, 1].set_ylim(axs[1, 0].get_ylim())
-    axs[0, 1].legend(["truth", "prediction"])
-    axs[1, 0].text(
-        date2num(y["trai"].index[y["trai"].shape[0] // 2]),
-        0.9 * axs[1, 0].get_ylim()[1],
-        "training",
-        horizontalalignment="center",
-    )
-    axs[1, 0].text(
-        date2num(y["vali"].index[y["vali"].shape[0] // 2]),
-        0.9 * axs[1, 0].get_ylim()[1],
-        "validation",
-        horizontalalignment="center",
-    )
-    axs[1, 0].text(
-        date2num(y["test"].index[y["test"].shape[0] // 2]),
-        0.9 * axs[1, 0].get_ylim()[1],
-        "testing",
-        horizontalalignment="center",
-    )
-    axs[1, 1].text(
-        date2num(y["trai"].index[y["trai"].shape[0] // 2]),
-        0.9 * axs[1, 0].get_ylim()[1],
-        "training",
-        horizontalalignment="center",
-    )
-    axs[1, 1].text(
-        date2num(y["vali"].index[y["vali"].shape[0] // 2]),
-        0.9 * axs[1, 0].get_ylim()[1],
-        "validation",
-        horizontalalignment="center",
-    )
-    axs[1, 1].text(
-        date2num(y["test"].index[y["test"].shape[0] // 2]),
-        0.9 * axs[1, 0].get_ylim()[1],
-        "testing",
-        horizontalalignment="center",
-    )
-    axs[0, 0].set_xticks([])
-    axs[0, 1].set_xticks([])
-    axs[1, 0].set_xlabel("time")
-    axs[1, 1].set_xlabel("time")
-    axs[0, 0].set_ylabel("normalised price")
-    axs[1, 0].set_ylabel("loss")
-    axs[0, 0].set_title("Linear Model")
-    axs[0, 1].set_title("LSTM Model")
-    fig.subplots_adjust(hspace=0)
+        for d in datasets:
+            pred[d, m] = _read_csv(output_dir, f"pred_{d}_{m}.csv")
+            loss[d, m] = _read_csv(output_dir, f"loss_{d}_{m}.csv")
+    _plot_scatter(output_dir, datasets, models, y, pred)
+    _plot_loss(output_dir, datasets, models, y, loss)
--- capstone/src/preprocessor.py
+++ capstone/src/preprocessor.py
@@ -1,35 +1,33 @@
 import os
-import pickle
+# import pickle
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
+# from sklearn.preprocessing import StandardScaler
 
 
-def macd(df: pd.DataFrame, n_fast=12, n_slow=26) -> pd.DataFrame:
-    EMAfast = df["vwap"].ewm(span=n_fast, min_periods=n_slow).mean()
-    EMAslow = df["vwap"].ewm(span=n_slow, min_periods=n_slow).mean()
-    MACD = pd.Series(EMAfast - EMAslow, name="macd_" + str(n_fast) + "_" + str(n_slow))
-    MACDsign = pd.Series(
-        MACD.ewm(span=9, min_periods=9).mean(),
-        name="macd_sign_" + str(n_fast) + "_" + str(n_slow),
+def macd(prices: pd.Series, n_fast=12, n_slow=26) -> pd.DataFrame:
+    EMAfast = prices.ewm(span=n_fast, min_periods=n_slow).mean()
+    EMAslow = prices.ewm(span=n_slow, min_periods=n_slow).mean()
+    MACD = EMAfast - EMAslow
+    MACDsign = MACD.ewm(span=9, min_periods=9).mean()
+    MACDdiff = MACD - MACDsign
+    return pd.DataFrame(
+        {
+            "macd_" + str(n_fast) + "_" + str(n_slow): MACD,
+            "macd_sign_" + str(n_fast) + "_" + str(n_slow): MACDsign,
+            "macd_diff_" + str(n_fast) + "_" + str(n_slow): MACDdiff,
+        }
     )
-    MACDdiff = pd.Series(
-        MACD - MACDsign, name="macd_diff_" + str(n_fast) + "_" + str(n_slow)
-    )
-    df = df.join(MACD)
-    df = df.join(MACDsign)
-    df = df.join(MACDdiff)
-    return df
 
 
-def rsi(df: pd.DataFrame, n=14) -> pd.DataFrame:
+def rsi(high: pd.Series, low: pd.Series, n=14) -> pd.Series:
     i = 0
     UpI = [0]
     DoI = [0]
-    while i + 1 < df.shape[0]:
-        UpMove = df.iloc[i + 1].high - df.iloc[i].high
-        DoMove = df.iloc[i].low - df.iloc[i + 1].low
+    while i + 1 < low.shape[0]:
+        UpMove = high.iloc[i + 1] - high.iloc[i]
+        DoMove = low.iloc[i] - low.iloc[i + 1]
         if UpMove > DoMove and UpMove > 0:
             UpD = UpMove
         else:
@@ -41,43 +39,61 @@ def rsi(df: pd.DataFrame, n=14) -> pd.DataFrame:
             DoD = 0
         DoI.append(DoD)
         i = i + 1
-    UpI = pd.Series(UpI, index=df.index)
-    DoI = pd.Series(DoI, index=df.index)
-    PosDI = pd.Series(UpI.ewm(span=n, min_periods=n).mean())
-    NegDI = pd.Series(DoI.ewm(span=n, min_periods=n).mean())
+    PosDI = pd.Series(UpI, index=low.index).ewm(span=n, min_periods=n).mean()
+    NegDI = pd.Series(DoI, index=low.index).ewm(span=n, min_periods=n).mean()
     RSI = pd.Series(PosDI / (PosDI + NegDI), name="rsi_" + str(n))
-    df = df.join(RSI)
-    return df
+    return RSI
 
 
 def preprocess(df: pd.DataFrame, input_dir: str) -> None:
-    df_x = df.drop(["symbol", "volume", "open", "lastSize"], axis="columns")
-    while pd.isna(df_x.iloc[0].vwap):
-        df_x = df_x.iloc[1:]
-    df_x["vwap"].fillna(method="ffill", inplace=True)
-    df_x = macd(df_x)
-    df_x = rsi(df_x)
+    df_x = pd.DataFrame(
+        {
+            "vwap_log": df.vwap.apply(pd.np.log),
+            "high_log": df.high.apply(pd.np.log),
+            "low_log": df.low.apply(pd.np.log),
+            "close_log": df.close.apply(pd.np.log),
+        }
+    )
+    df_x.vwap_log.fillna(method="ffill", inplace=True)
+    df_x["vwap_log_diff"] = df_x.vwap_log - df_x.vwap_log.shift()
+    df_x["high_log_diff"] = df_x.high_log - df_x.vwap_log.shift()
+    df_x["low_log_diff"] = df_x.low_log - df_x.vwap_log.shift()
+    df_x["close_log_diff"] = df_x.close_log - df_x.vwap_log.shift()
+    df_x.drop(
+        ["vwap_log", "high_log", "low_log", "close_log"], axis="columns", inplace=True
+    )
+    df_x = df_x.join(
+        pd.DataFrame(
+            {
+                "trades_log": (df.trades + 1).apply(pd.np.log),
+                "home_notional_log": (df.homeNotional + 1).apply(pd.np.log),
+                "foreign_notional_log": (df.foreignNotional + 1).apply(pd.np.log),
+            }
+        )
+    )
+    df_x = df_x.join(macd(df.vwap))
+    df_x = df_x.join(rsi(df.high, df.low))
     while pd.isna(df_x.iloc[0].macd_sign_12_26):
         df_x = df_x.iloc[1:]
 
-    scaler = StandardScaler()
-    df_x = pd.DataFrame(
-        scaler.fit_transform(df_x.to_numpy()), columns=df_x.columns, index=df_x.index
-    )
+    # scaler = StandardScaler()
+    # df_x = pd.DataFrame(
+    #     scaler.fit_transform(df_x.to_numpy()), columns=df_x.columns, index=df_x.index
+    # )
 
-    df_y = pd.Series(df_x["vwap"].shift(-1), name="next")
+    df_y = pd.Series(df_x["vwap_log_diff"].shift(-1), name="next")
     df_x = df_x[:-1]
     df_y = df_y[:-1]
 
-    x_trai, x_vali = train_test_split(df_x, test_size=0.4, shuffle=False)
-    x_vali, x_test = train_test_split(x_vali, test_size=0.5, shuffle=False)
-    y_trai, y_vali = train_test_split(df_y, test_size=0.4, shuffle=False)
-    y_vali, y_test = train_test_split(y_vali, test_size=0.5, shuffle=False)
+    trai_x, vali_x = train_test_split(df_x, test_size=0.4, shuffle=False)
+    vali_x, test_x = train_test_split(vali_x, test_size=0.5, shuffle=False)
+    trai_y, vali_y = train_test_split(df_y, test_size=0.4, shuffle=False)
+    vali_y, test_y = train_test_split(vali_y, test_size=0.5, shuffle=False)
 
-    x_trai.to_csv(os.path.join(input_dir, "trai_x.csv"))
-    x_vali.to_csv(os.path.join(input_dir, "vali_x.csv"))
-    x_test.to_csv(os.path.join(input_dir, "test_x.csv"))
-    y_trai.to_csv(os.path.join(input_dir, "trai_y.csv"), header=True)
-    y_vali.to_csv(os.path.join(input_dir, "vali_y.csv"), header=True)
-    y_test.to_csv(os.path.join(input_dir, "test_y.csv"), header=True)
-    pickle.dump(scaler, open(os.path.join(input_dir, "scaler.pkl"), "wb"))
+    trai_x.to_csv(os.path.join(input_dir, "trai_x.csv"))
+    vali_x.to_csv(os.path.join(input_dir, "vali_x.csv"))
+    test_x.to_csv(os.path.join(input_dir, "test_x.csv"))
+    trai_y.to_csv(os.path.join(input_dir, "trai_y.csv"), header=True)
+    vali_y.to_csv(os.path.join(input_dir, "vali_y.csv"), header=True)
+    test_y.to_csv(os.path.join(input_dir, "test_y.csv"), header=True)
+    # pickle.dump(scaler, open(os.path.join(input_dir, "scaler.pkl"), "wb"))
